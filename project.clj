ca8eba095d2e5a5246f2e320220669fe00e096f6
Peter Kehl <peter.kehl@gmail.com>
2018-11-07 09:36:55 -0800

    Skeleton

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject dbg "0.1.0-SNAPSHOT"
+  :description "Tracing evaluation order"
+  :url "http://example.com/FIXME"
+  :license {:name "GNU GPL v. 3"
+            :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
---
+++ src/dbg/main.clj
@@ -0,0 +1,6 @@
+(ns dbg.main)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
